13d92413f2e3a6b51899e80889cb8f8fd1d9ee1b
Ansh7272 <120785449+Ansh7272@users.noreply.github.com>
2025-10-04 23:53:03 +0530

    created allrooms,roomdetails page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
 
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -9,14 +12,17 @@ import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
 
-  // const isownerPath =useLocation().pathname.includes("owner")
+  const isownerPath =useLocation().pathname.includes("owner")
   return (
     <>
-      <Nav />
+      {!isownerPath && <Nav />}
       <div className='min-h-[70vh]'>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/rooms" element={<AllRooms/>} />
+        <Route path="/rooms/:id" element={<RoomDetails/>} />
       </Routes>
+      <Footer/>
       </div>
       
       </>
